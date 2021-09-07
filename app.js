e6380ffff68cbdba86302ce115fcb369eb5b1a71
rohith-thalla <89257871+rohith-thalla@users.noreply.github.com>
2021-09-07 12:39:48 +0530

    initial

---
+++ app.js
@@ -0,0 +1,23 @@
+var input = document.querySelector("#int");
+var output = document.querySelector("#out");
+var button = document.querySelector("#btn");
+var serverURL = "https://api.funtranslations.com/translate/ferb-latin.json"
+
+function servercall(text){
+    return serverURL + "?" + "key=" + text;
+}
+
+function errorhandling(error){
+    console.log("error occured " + error)
+    alert("Maximum server requests received, try again in an hour")
+}
+
+function clickeventhandler(){
+    var inputtext = input.value;
+    fetch(servercall(inputtext))
+        .then(response => response.json())
+        .then(json => output.innerText= json.contents.translated)
+        .catch(errorhandling)
+};
+alert("The server is restricted to only 5 trials per hour if you encounter an error try again after an hour")
+button.addEventListener("click",clickeventhandler)
